ea090c9a2ca12807a313b8f33be7ba89d58a8ecd
Tate Jeppesen <jeppesenlelouch@gmail.com>
2017-06-27 20:07:45 -0600

    added form

---
+++ client/src/components/AppForm.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import addApp from '../actions/apps'
+import updateApp from '../actions/apps'
+import deleteApp from '../actions/apps'
+
+class AppForm extends React.Component {
+  state = {
+    name: '',
+    description: '',
+    category: '',
+    price: '',
+    author: '',
+    version: '',
+  }
+
+    submit = (e) => {
+      e.preventDefault();
+      let { dispatch } = this.props;
+      let { name, description, category, price, author, version } = this.state
+      let blog = { name, description, category, price, author, version }
+      dispatch(AddApp(blog))
+    }
+
+    render() {
+      let { name, description, category, price, author, version} = this.state;
+      return (
+        <form onSubmit(this.submit)>
+        <input
+          placeholder='Name'
+          value={name}
+        />
+        <input
+          placeholder='Description'
+          value={description}
+        />
+        <input
+          placeholder='Price'
+          value={price}
+        />
+        <input
+          placeholder='Author'
+          value={author}
+        />
+        <input
+          placeholder='Version'
+          value={version}
+        />
+        </form>
+      )
+    }
+}
+
+export default AppForm;
---
+++ client/src/components/AppView.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Appview = () => (
+  <div></div>
+)
+
+export default Appview;
